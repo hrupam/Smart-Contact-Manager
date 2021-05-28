c7af9f9bf879c659aa3ab43fc06a476f46a0df7a
Rupam Hari <hrupam55@gmail.com>
2021-05-29 01:32:40 +0530

    initial commit

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,90 @@
+package com.smart.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "SCM | Home");
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "SCM | About");
+		return "about";
+	}
+
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title", "SCM | Signup");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("title", "SCM | Login");
+		return "login";
+	}
+
+	@PostMapping("/user_registration")
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult bindingresult,
+			@RequestParam(name = "agreement", defaultValue = "false") boolean agreement,
+			@RequestParam(name = "confirm_password", defaultValue = "") String confirmPassword, Model m,
+			HttpSession session) {
+
+		try {
+			if (!agreement) {
+				throw new Exception("You must agree with T&C");
+			}
+			if (bindingresult.hasErrors()) {
+				System.out.println(bindingresult);
+				return "signup";
+			}
+			if (confirmPassword.isBlank()) {
+				throw new Exception("Please confirm your password");
+			} else if (!(user.getPassword()).equals(confirmPassword))
+				throw new Exception("Password didn't match");
+
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png");
+
+			this.userRepository.save(user);
+
+			session.setAttribute("message", new Message("Successfully registered", "alert-success"));
+			return "signup";
+
+		} catch (DataIntegrityViolationException e) {
+			session.setAttribute("message", new Message("Email address already exists", "alert-danger"));
+			return "signup";
+		} catch (Exception e) {
+			System.out.println(e);
+			session.setAttribute("message", new Message(e.getMessage(), "alert-danger"));
+			return "signup";
+
+		}
+
+	}
+}
---
+++ src/main/java/com/smart/entities/User.java
@@ -0,0 +1,130 @@
+package com.smart.entities;
+
+import java.util.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "USER")
+public class User {
+
+	public User() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@NotBlank(message = "Name cannot be blank !")
+	@Size(min = 2, message = "Name must be minimum of 2 characters !")
+	private String name;
+
+	@Column(unique = true)
+	@Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Invalid Email")
+	private String email;
+
+	@NotBlank(message = "Password cannot be blank !")
+	private String password;
+	private String role;
+	private String imageUrl;
+
+	@Column(length = 500)
+	@Size(min = 3, message = "About must be minimum of 3 characters")
+	private String about;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user")
+	private List<Contact> contacts = new ArrayList<>();
+
+	private boolean isEnabled;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public String getAbout() {
+		return about;
+	}
+
+	public void setAbout(String about) {
+		this.about = about;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", role=" + role
+				+ ", imageUrl=" + imageUrl + ", about=" + about + ", contacts=" + contacts + ", isEnabled=" + isEnabled
+				+ "]";
+	}
+
+}
